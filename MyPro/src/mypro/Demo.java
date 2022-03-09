64b5b5308fd015701123af557e8aa8eff4991717
Shetty, Pragati <shetty.pragati@principal.com>
2022-03-09 13:05:01 +0530

    Java Project with jdk 1.4 has been created by Mr. A

---
+++ MyPro/src/mypro/Demo.java
@@ -0,0 +1,17 @@
+package mypro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List mylist = new ArrayList();
+		mylist.add(100);
+		mylist.add("hello");
+		mylist.add(3.4);
+		System.out.println(mylist);
+	}
+
+}
